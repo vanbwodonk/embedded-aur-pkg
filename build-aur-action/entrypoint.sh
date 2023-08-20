0c0bbddd6c4fee0f430a1c677b90e7036d08622f
Arif Darmawan <arif.pens@gmail.com>
2023-08-20 21:08:48 +0700

    Update entrypoint.sh to auto detect folder

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -19,5 +19,7 @@ if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
 fi
 
 sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"
-cd "./$pkgname" || exit 1
+cdls() { cd "$@" && ls; }
+cdls . || exit 1
+#cd "./$pkgname" || exit 1
 python3 ../build-aur-action/encode_name.py
