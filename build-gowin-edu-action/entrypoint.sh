be9c9388676fcd552f97530d1e7801181d14a593
Arif Darmawan <arif.pens@gmail.com>
2023-10-16 08:52:31 +0700

    split gowin eda & eda-edu actions

--- build-gowin-edu-action/entrypoint.sh
+++ build-gowin-edu-action/entrypoint.sh
@@ -19,5 +19,5 @@ if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
 fi
 
 sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"
-cd "./gowin-eda" || exit 1
+cd "./IDE" || exit 1
 python3 ../build-aur-action/encode_name.py
