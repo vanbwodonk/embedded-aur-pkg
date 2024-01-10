d90d4f7e8e6192724e805458987e823dbef40588
Arif Darmawan <arif.pens@gmail.com>
2024-01-10 16:17:18 +0700

    Update entrypoint.sh
    try disable archlinuxcn repo

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -6,10 +6,10 @@ useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
-cat << EOM >> /etc/pacman.conf
-[archlinuxcn]
-Server = https://repo.archlinuxcn.org/x86_64
-EOM
+#cat << EOM >> /etc/pacman.conf
+#[archlinuxcn]
+#Server = https://repo.archlinuxcn.org/x86_64
+#EOM
 
 pacman-key --init
 pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
